94954b607923a45c9b30e5bbd4b023979adfa63b
Thomas Charpentier <thomas.charpentier@etu.enseeiht.fr>
2012-03-07 19:05:01 +0100

    DONE: registration (without DB)

--- app/server_authentication.rb
+++ app/server_authentication.rb
@@ -1,13 +1,32 @@
 require 'sinatra'
 
-get '/session/register' do
-  'Hello World'
+set :user_table, {}
+
+helpers do
+  #We just use a simple global variable to store user accounts
+  #A true db will come after
+  def find_user(user)
+    settings.user_table[user]
+  end
+  def add_user(user, password)
+    settings.user_table[user] = { 'password' => password }
+  end
+end
+
+get '/register' do
+  erb :register
 end
 
 post '/registered' do
-  if params[:login] && params[:password]
-    #TODO
+  
+  if params['login'] && params['password']
+    if find_user(params['login'])
+      redirect '/register'
+    else
+      add_user(params['login'],params['password'])
+      redirect '/session/new'
+    end
   else
-    redirect '/session/register'
+    redirect '/register'
   end
 end
--- spec/server_authentication_spec.rb
+++ spec/server_authentication_spec.rb
@@ -11,13 +11,51 @@ describe 'server_authentication' do
 
   describe 'registration' do
 
-    context 'Client send a post without a login or a password at url /registered' do
-      it 'should redirect to /session/register' do
-        post '/registered'
-        last_response.status.should be 302
+    context 'Client send an invalid post /registered' do
+      context 'with a password, without a login' do
+        it 'should redirect to /register' do
+          post '/registered', {'password' => '1234'}
+          last_response.status.should be 302
+          last_response.header['Location'].should match(/^http:\/\/[^\/]*\/register$/)
+        end
       end
+      context 'without a password, with a login' do
+        it 'should redirect to /register' do
+          post '/registered', {'login' => 'toto'}
+          last_response.status.should be 302
+          last_response.header['Location'].should match(/^http:\/\/[^\/]*\/register$/)
+        end
+      end
+      context 'without a password, without a login' do
+        it 'should redirect to /register' do
+          post '/registered', {}
+          last_response.status.should be 302
+          last_response.header['Location'].should match(/^http:\/\/[^\/]*\/register$/)
+        end
+      end
+      context 'with a password, with a login already use' do
+        it 'should redirect to /register' do
+          post '/registered', {'login' => 'toto', 'password' => '1234'}
+          post '/registered', {'login' => 'toto', 'password' => '4321'}
+          last_response.status.should be 302
+          last_response.header['Location'].should match(/^http:\/\/[^\/]*\/register$/)
+        end
+      end
+      context 'Client send a valid post /registered' do
+        context 'with a password, with a free login' do
+          it 'should redirect to /session/new' do
+            post '/registered', {'login' => 'titi', 'password' => '4321'}
+            last_response.status.should be 302
+            last_response.header['Location'].should match(/^http:\/\/[^\/]*\/session\/new$/)
+          end
+          it 'should store the user login and password' do
+            #TODO, I don't know how to do that
+          end
+        end
+      end
+
     end
     
   end
-
+  
 end
